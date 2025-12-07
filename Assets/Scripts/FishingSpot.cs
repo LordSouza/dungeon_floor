7472dcd2f20b2033c5294523e359ea337f719362
Lucas Oliveira <79264145+LordSouza@users.noreply.github.com>
2025-12-07 09:46:05 +0000

    Add functionality to track and update fishing spot availability status

---
+++ Assets/Scripts/FishingSpot.cs
@@ -0,0 +1,72 @@
+using TMPro;
+using UnityEngine;
+
+public class FishingSpot : MonoBehaviour
+{
+    public FishingMinigame fishingMinigame;
+    public TextMeshProUGUI promptText; // Optional: "Press E to Fish"
+    public KeyCode interactKey = KeyCode.E;
+    
+    private bool playerInRange = false;
+    
+    void Start()
+    {
+        if (fishingMinigame == null)
+        {
+            fishingMinigame = FindObjectOfType<FishingMinigame>();
+        }
+        
+        if (promptText != null)
+        {
+            promptText.gameObject.SetActive(false);
+        }
+    }
+    
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(interactKey))
+        {
+            StartFishing();
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            if (promptText != null)
+            {
+                promptText.gameObject.SetActive(true);
+                promptText.text = $"Press {interactKey} to Fish";
+            }
+            Debug.Log("Player entered fishing spot");
+        }
+    }
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            if (promptText != null)
+            {
+                promptText.gameObject.SetActive(false);
+            }
+            Debug.Log("Player left fishing spot");
+        }
+    }
+    
+    void StartFishing()
+    {
+        if (fishingMinigame != null)
+        {
+            fishingMinigame.StartFishing();
+            Debug.Log("Started fishing minigame!");
+        }
+        else
+        {
+            Debug.LogError("FishingMinigame not found!");
+        }
+    }
+}
